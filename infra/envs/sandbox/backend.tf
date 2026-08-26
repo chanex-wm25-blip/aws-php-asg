6ac6041e8d80cf4edc39bf52bfe1317851e2066b
jaclynnn29 <angelinajaclyn00@gmail.com>
2026-08-26 11:02:55 +0800

    running for 2nd time

--- infra/envs/sandbox/backend.tf
+++ infra/envs/sandbox/backend.tf
@@ -10,10 +10,10 @@
 # name is only account-scoped, so "assignment-tf-lock" is fine as-is.
 terraform {
   backend "s3" {
-    bucket         = "assignment-tfstate-609329194143"
+    bucket         = "shuttlebus-tfstate-609329194143"
     key            = "sandbox/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "assignment-tf-shuttlebus"
+    dynamodb_table = "assignment-tf-shuttlebus-lock"
     encrypt        = true
   }
 }
