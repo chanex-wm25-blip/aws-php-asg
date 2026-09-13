b6d9d76951afb62dec161296fc30a6552b353262
jaclynnn29 <angelinajaclyn00@gmail.com>
2026-09-13 11:24:54 +0800

    trying to fix s3 photo

--- infra/modules/s3/main.tf
+++ infra/modules/s3/main.tf
@@ -41,7 +41,7 @@ resource "aws_s3_bucket_policy" "public_read" {
         Effect    = "Allow"
         Principal = "*"
         Action    = "s3:GetObject"
-        Resource  = "arn:aws:s3:::${var.bucket_name}/${var.public_read_prefix}"
+        Resource = "arn:aws:s3:::${var.bucket_name}/${var.public_read_prefix}*"
       }
     ]
   })
