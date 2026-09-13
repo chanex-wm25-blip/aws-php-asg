9b23aca5e819707517bcd193378f1e1ba51357c8
jaclynnn29 <angelinajaclyn00@gmail.com>
2026-09-13 22:18:56 +0800

    change back to false

--- infra/modules/rds/main.tf
+++ infra/modules/rds/main.tf
@@ -30,7 +30,7 @@ resource "aws_db_instance" "this" {
   publicly_accessible = false
 
   # Allow the sandbox to be destroyed while retaining a final snapshot.
-  skip_final_snapshot       = true
+  skip_final_snapshot       = false
   final_snapshot_identifier = "${var.name_prefix}-rds-final"
   backup_retention_period   = 7
   deletion_protection       = false
