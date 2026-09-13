a1075c78b4ec91dc2af3b550bc3c38952e0fea5c
jaclynnn29 <angelinajaclyn00@gmail.com>
2026-09-13 11:10:14 +0800

    add s3

--- infra/modules/s3/main.tf
+++ infra/modules/s3/main.tf
@@ -55,7 +55,7 @@ resource "aws_s3_bucket_cors_configuration" "uploads" {
   depends_on = [terraform_data.uploads]
 
   cors_rule {
-    allowed_headers = [""]
+    allowed_headers = ["*"]
     allowed_methods = ["GET"]
     allowed_origins = ["*"]
     max_age_seconds = 3000
